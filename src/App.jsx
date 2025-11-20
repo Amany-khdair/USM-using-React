726eca51f2970d6d963c7fa32f4ce86768095f35
Amany-khdair <amany.khdair0@gmail.com>
2025-11-20 15:30:25 +0200

    SPA

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import Footer from './components/footer/Footer'
+import Navbar from './components/navbar/Navbar'
+import Home from './pages/home/Home'
+import Users from './pages/user/Users'
+import User from './pages/user/User'
+import AddUser from './pages/user/AddUser'
 
 function App() {
 
   return (
     <>
-      
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/users' element={<Users/>}/>
+        <Route path='/user' element={<User/>}/>
+        <Route path='/users/add' element={<AddUser/>}/>
+      </Routes>
+      <Footer/>
     </>
   )
 }
